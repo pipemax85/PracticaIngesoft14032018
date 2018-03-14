6d9150ca6ed75be0f7bb5aba79fa19005db24713
Felipe Ramos <cframosl@unal.edu.co>
2018-03-14 20:59:36 +0000

    practica2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsControler < ApplicationControler
+    def create 
+        user = User.where(email: params [:email]).first
+        
+        if user.valid_password?(params[:password])
+            render json: user.as_json(only : [:email, :authentication_token]), status
+        else 
+            head(:unauthorized)
+    end
+    def destroy
+    end
+end
